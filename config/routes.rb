2da2c37b864c0594a84c8001338dbddec17a9702
Chris Birie <chris.birie@gmail.com>
2010-02-25 13:41:40 -0500

    added routes again again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root controller => 'teams', :action => 'index'
+  map.root :controller => "teams", :action => "index"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
